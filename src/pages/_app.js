f1723c845a3bc2b41b925aa99ba814f6ad6f285e
mostafa <mostafa.h.web@gmail.com>
2023-11-30 00:47:56 +0300

    add bootstrap to the project

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,22 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
 import '@/styles/globals.css'
+import HeaderComponent from '../../components/header'
+import { useEffect } from 'react'
+
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+
+  useEffect(() => {
+    require('bootstrap/dist/js/bootstrap')
+  },[])
+
+
+
+  return(
+    <>
+    <HeaderComponent/>
+    <Component {...pageProps} />
+    </>
+  ) 
 }
