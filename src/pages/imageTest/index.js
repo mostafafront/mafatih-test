78dbd6e55708916ae0509202f1c79ce64a46b53b
mostafa <mostafa.h.web@gmail.com>
2024-02-10 17:52:37 +0300

    latest commit

--- src/pages/imageTest/index.js
+++ src/pages/imageTest/index.js
@@ -1,33 +1,79 @@
-import Image from 'next/image'
-import React from 'react'
+import Image from "next/image";
+import React, { useEffect } from "react";
 
-import logo from '../../../public/images/logo.png'
-import darkTheme from '../../../public/images/darkTheme.png'
+import logo from "../../../public/images/logo.png";
+import darkTheme from "../../../public/images/darkTheme.png";
+import footerPrayer from "../../../public/images/footerPrayer.svg";
 
-import link from '../../../jsonApi/link.json'
+import link from "../../../jsonApi/link.json";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function ImageTest() {
 
+  // let firstTag = link.body.indexOf(">");
+  // let secondTag = link.body.indexOf("</a>");
+  
+  // let href = link.body.indexOf("href='");
+  // let hrefEnd = link.body.indexOf("'>");
+  // let hrefStr = link.body.slice(href +6, hrefEnd)
+
+  // let aTag = link.body.slice(firstTag +1, secondTag);
+
+  const router = useRouter();
+  useEffect(() => {
+    const Atag = document.querySelectorAll("a");
+
+    Atag.forEach((itemA) => {
+      itemA.addEventListener("click",  (e) => {
+        e.preventDefault();
+        router.push(itemA.href)
+      })
+    })
+
+  },[])
+
+
+
   return (
     <>
+   
+      <p dangerouslySetInnerHTML={{ __html: link.body }}></p>
 
 
-    <p dangerouslySetInnerHTML={{__html: link.body}}></p>
-    <div className='box'>
-        <Image src={'/images/imam-hossein.jpg'} alt='logo' fill objectFit='cover' priority/>
-    </div>
-    <div>
-        <Image src={logo} alt='logo' sizes="(max-width: 768px) 100px, (max-width: 1200px) 100vw, 33vw" />
-    </div> 
-    <div>
-        <Image src={logo} alt='logo' width={120} quality={100}/>
-    </div>
-    <div>
-        <Image src={darkTheme} alt='logo' width={222} height={160}/>
-    </div>
-    <div>
-        <img src={'/images/darkTheme.png'} alt='logo'  width={222} height={160}/>
-    </div>
+      <div className="box">
+        <Image
+          src={"/images/imam-hossein.jpg"}
+          alt="logo"
+          fill
+          objectFit="cover"
+          priority
+        />
+      </div>
+      <div>
+        <Image
+          src={logo}
+          alt="logo"
+          sizes="(max-width: 768px) 100px, (max-width: 1200px) 100vw, 33vw"
+        />
+      </div>
+      <div>
+        <Image src={footerPrayer} alt="logo" />
+      </div>
+      <div>
+        <Image src={logo} alt="logo" width={120} quality={100} />
+      </div>
+      <div>
+        <Image src={darkTheme} alt="logo" width={222} height={160} />
+      </div>
+      <div>
+        <img
+          src={"/images/darkTheme.png"}
+          alt="logo"
+          width={222}
+          height={160}
+        />
+      </div>
     </>
-  )
+  );
 }
